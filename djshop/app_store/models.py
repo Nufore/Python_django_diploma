2648de8871a63394f06364146cd853b74f343758
Nufore <werd322@yandex.ru>
2023-01-29 19:08:59 +0300

    created DB models

--- djshop/app_store/models.py
+++ djshop/app_store/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(ProductCategory, null=False, on_delete=models.CASCADE)
+    picture = models.ImageField(default=None, null=True, upload_to='product_pictures/')
+    description = models.CharField(max_length=300)
+    price = models.FloatField(default=0.0, null=False)
+    feedback_count = models.IntegerField(default=0, null=False)
+    added_at = models.DateTimeField(auto_now_add=True)
+    manufacturer = models.CharField(max_length=200)
+
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    text = models.TextField(null=False)
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CartList(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+
+
+class PaymentType(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=20)
+
+
+class PaymentStatus(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Payment(models.Model):
+    type = models.ForeignKey(PaymentType, on_delete=models.CASCADE)
+    status = models.ForeignKey(PaymentStatus, on_delete=models.CASCADE)
+    error_message = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=8)
+
+
+class Delivery(models.Model):
+    name = models.CharField(max_length=20)
+    is_express = models.BooleanField(default=False)
+    express_price = models.FloatField(default=500.0, null=True)
+    base_less_than = models.FloatField(default=2000.0, null=True)
+    base_price = models.FloatField(default=200, null=True)
+
+
+class Order(models.Model):
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_cost = models.FloatField(default=0.0, null=False)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+    delivery_type = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+
+
+class OrderList(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
--- djshop/app_users/models.py
+++ djshop/app_users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+    account_balance = models.FloatField(default=0.0, null=False)
+    avatar = models.ImageField(default=None, null=True, upload_to='avatars/')
